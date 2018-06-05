7c09fdb001a2f661856ca345acbaef809c2e9b4f
Ravichandran <ravi.nila@live.com>
2018-06-05 16:51:48 +0530

    Create find-sub-array-list-sum-equals-given-sum-value.js

---
+++ JavaScript/find-sub-array-list-sum-equals-given-sum-value.js
@@ -0,0 +1,49 @@
+
+/* 
+
+Find if sequence of array numbers which meets the given sum
+
+
+Input: 
+
+  arr[] = [11, -2, 9, -3, 5, 6];
+  sum = 20
+
+Output: 
+
+  { has: true, subarray: [ 11, -2, 9, -3, 5 ] }
+  
+
+  Here when you sum this sequence [ 11, -2, 9, -3, 5 ]
+  We get the sum 20
+
+*/
+
+function hasSumSequence(arr, sum){
+
+  var tempSum = 0, len = arr.length;
+  
+  
+  for(var i = 0, j = 0; i < len; i++){
+    tempSum += arr[i];
+
+    if(tempSum > sum){
+      tempSum -= arr[j];
+      j++;
+    }
+    
+    if (tempSum === sum){
+      return {has: true, subarray: arr.slice(j, i+1)};
+    }
+    
+  }
+  
+  return {has: false};
+}
+
+
+/* Sample input/output */
+
+var arr = [11, -2, 9, -3, 5, 6];
+
+console.log(hasSumSequence(arr, 20));
